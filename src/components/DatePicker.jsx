a6ec1721e44f41a4a1a952d06a9dab7eed1b87ce
DartaBlenke <dartagnan.blenke17@gmail.com>
2023-11-27 20:43:34 -0300

    datetimepicker - style ajuste

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -3,13 +3,14 @@ import 'react-datetime-picker/dist/DateTimePicker.css'
 import 'react-calendar/dist/Calendar.css';
 import 'react-clock/dist/Clock.css';
 import DateTimePicker from 'react-datetime-picker'
+import '../styles/DateTimePicker.css'
 
 export const Datepicker = () => {
   const [selectedDate, setSelectedDate] = useState(new Date())
 
   return (
     <label className='relative cursor-pointer'>
-      <DateTimePicker value={selectedDate} onChange={setSelectedDate} className='h-20 w-full px-4 text-2xl text-gray-700 border-[#9db8fb] border-2 rounded-lg border-opacity-50 outline-none focus:border-blue-500 placeholder-gray-300 placeholder-opacity-0 transition duration-200'/>
+      <DateTimePicker disableClock={true} disableCalendar={true} minDate={new Date()} value={selectedDate} onChange={setSelectedDate} className='h-20 w-full px-8 sm:px-20 text-xl text-gray-700 border-[#9db8fb] border-2 rounded-lg border-opacity-50 outline-none focus:border-blue-500 placeholder-gray-300 placeholder-opacity-0 transition duration-200'/>
     </label>
   )
 }
