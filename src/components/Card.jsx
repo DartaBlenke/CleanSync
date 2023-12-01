69d81990c587bb4b17a55e1761d6dedeb923949b
DartaBlenke <dartagnan.blenke17@gmail.com>
2023-12-01 15:07:27 -0300

    teste titulos client

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function Card ({item, selectVehicle}) {
   return (
     <div onClick={() => selectVehicle(item)} className='w-[55%] relative max-x-xs overflow-hidden rounded-2xl shadow-xl border-4 border-blue-900/50 hover:border-blue-900'>
--- src/routes/__tests__/Client.test.js
+++ src/routes/__tests__/Client.test.js
@@ -1,3 +1,42 @@
-test('it works', () => {
-  expect(true).toBe(true);
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Client from '../Client';
+
+// Mock para o useNavigate
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(),
+}));
+
+describe('Client', () => {
+  beforeAll(() => {
+    // Simulando um valor para localStorage.getItem('vehicles') antes dos testes
+    const mockVehicles = JSON.stringify([{ id: 1, name: 'Carro 1' }, { id: 2, name: 'Carro 2' }]);
+    Object.defineProperty(window, 'localStorage', {
+      value: {
+        getItem: jest.fn().mockReturnValue(mockVehicles),
+      },
+      configurable: true,
+      writable: true,
+    });
+  });
+
+  test('renders the main title', () => {
+    render(
+      <Router>
+        <Client />
+      </Router>
+    );
+    expect(screen.getByText('Bem vindo ao nosso serviço de agendamento!!')).toBeInTheDocument();
+  });
+
+  test('renders the subtitle', () => {
+    render(
+      <Router>
+        <Client />
+      </Router>
+    );
+    expect(screen.getByText('Por favor, informe qual o tipo do seu automóvel.')).toBeInTheDocument();
+  });
 });
