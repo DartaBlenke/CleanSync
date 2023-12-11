5202ea0e5b350683c79f170857e979b02980d0a9
DartaBlenke <dartagnan.blenke17@gmail.com>
2023-12-11 19:26:11 -0300

    remocao ;

--- src/components/PayCard.jsx
+++ src/components/PayCard.jsx
@@ -9,7 +9,7 @@ export default function PayCard({ item, onClick, selected, setSelected }) {
     }
 
     onClick && onClick(item.id)
-  };
+  }
   return (
     <div onClick={handleClick} id={item.id} className={`flex flex-row gap-[20%] sm:gap-[30%] md:gap-[35%] items-center w-full h-20 px-4 relative max-x-xs overflow-hidden border-2 rounded-lg hover:border-blue-900 ${selected === true ? 'border-[#1c47b3]' : 'border-[#9db8fb]'}`}>
       <div className='text-xl'>
